177d6e9e73031b0b9eed4724652bcad9bb4e6222
Chaohui Xu <cx30@georgetown.edu>
2018-04-19 15:26:19 -0400

    test

--- R/functions.R
+++ R/functions.R
@@ -283,9 +283,10 @@ select_sig <- function(x = NULL, class_label = NULL, id = NULL,
     indeed_df <- cbind(pvalue, rowSums(abs(binary_link)), dn_score )
 
 
+    write.table(pvalue, file = "test.csv", sep = ',')
     # save the p-value, node degree and activity score of each biomarker candidate
-    write.table(indeed_df, file = "INDEED_result.csv", sep=",", quote = FALSE,
-        row.names = FALSE, col.names = c("ID", "P-value", "Node Degree", "Activity Score"))
+    ##write.table(indeed_df, file = "INDEED_result.csv", sep=",", quote = FALSE,
+       ## row.names = FALSE, col.names = c("ID", "P-value", "Node Degree", "Activity Score"))
 }
 
 
